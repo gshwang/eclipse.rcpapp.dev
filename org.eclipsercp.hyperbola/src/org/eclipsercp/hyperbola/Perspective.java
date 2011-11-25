2b690969cfe0979412979c4bbc86615e21850eaa
gshwang <java304@kornet.net>
2011-11-25 13:47:36 +0900

    add new class and image

--- org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/Perspective.java
+++ org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/Perspective.java
@@ -2,9 +2,12 @@ package org.eclipsercp.hyperbola;
 
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipsercp.hyperbola.view.ContactsView;
 
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		layout.addStandaloneView(ContactsView.ID, false, IPageLayout.LEFT, 1.0f, layout.getEditorArea());
 	}
 }
---
+++ org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/view/ContactsView.java
@@ -0,0 +1,23 @@
+package org.eclipsercp.hyperbola.view;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class ContactsView extends ViewPart {
+	
+	public static final String ID = 
+			"org.eclipsercp.hyperbola.view.contacts";
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
