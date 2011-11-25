1e7508d4195c649de36e4c75150d685107b06c54
gshwang <java304@kornet.net>
2011-11-25 10:16:01 +0900

    new

---
+++ org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/Perspective.java
@@ -0,0 +1,10 @@
+package org.eclipsercp.hyperbola;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
