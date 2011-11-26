42dbb3e505336c8058bbecd6442686d4db08e794
gshwang <java304@kornet.net>
2011-11-26 21:37:36 +0900

    recoding

--- org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/ApplicationActionBarAdvisor.java
+++ org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/ApplicationActionBarAdvisor.java
@@ -1,20 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Jean-Michel Lemieux, Jeff McAffer, Chris Aniszczyk and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Hyperbola is an RCP application developed for the book
+ *     Eclipse Rich Client Platform - 
+ *         Designing, Coding, and Packaging Java Applications
+ * See http://eclipsercp.org
+ *
+ * Contributors:
+ *     Jean-Michel Lemieux and Jeff McAffer - initial API and implementation
+ *     Chris Aniszczyk - edits for the second edition
+ *******************************************************************************/
 package org.eclipsercp.hyperbola;
 
+import org.eclipse.jface.action.ICoolBarManager;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
+import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
-    public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
-        super(configurer);
-    }
+	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+		super(configurer);
+	}
 
-    protected void makeActions(IWorkbenchWindow window) {
-    }
+	private IWorkbenchAction exitAction;
 
-    protected void fillMenuBar(IMenuManager menuBar) {
-    }
-    
+	private IWorkbenchAction aboutAction;
+
+
+	protected void makeActions(IWorkbenchWindow window) {
+		exitAction = ActionFactory.QUIT.create(window);
+		register(exitAction);
+		aboutAction = ActionFactory.ABOUT.create(window);
+		register(aboutAction);
+	}
+
+	protected void fillMenuBar(IMenuManager menuBar) {
+		MenuManager hyperbolaMenu = new MenuManager("&Hyperbola", "hyperbola");
+		hyperbolaMenu.add(new Separator());
+		hyperbolaMenu.add(exitAction);
+		MenuManager helpMenu = new MenuManager("&Help", "help");
+		helpMenu.add(aboutAction);
+		menuBar.add(hyperbolaMenu);
+		menuBar.add(helpMenu);
+	}
+
+	protected void fillCoolBar(ICoolBarManager coolBar) {
+		IToolBarManager toolbar = new ToolBarManager(coolBar.getStyle());
+		coolBar.add(toolbar);
+		toolbar.add(aboutAction);
+	}
+
+	protected void fillTrayItem(IMenuManager trayItem) {
+		trayItem.add(aboutAction);
+		trayItem.add(exitAction);
+	}
+	
 }
--- org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/ApplicationWorkbenchWindowAdvisor.java
+++ org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/ApplicationWorkbenchWindowAdvisor.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Jean-Michel Lemieux, Jeff McAffer, Chris Aniszczyk and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Hyperbola is an RCP application developed for the book
+ *     Eclipse Rich Client Platform - 
+ *         Designing, Coding, and Packaging Java Applications
+ * See http://eclipsercp.org
+ *
+ * Contributors:
+ *     Jean-Michel Lemieux and Jeff McAffer - initial API and implementation
+ *     Chris Aniszczyk - edits for the second edition
+ *******************************************************************************/
 package org.eclipsercp.hyperbola;
 
 import org.eclipse.swt.graphics.Point;
@@ -8,19 +24,22 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
-    public ApplicationWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        super(configurer);
-    }
+	public ApplicationWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		super(configurer);
+	}
 
-    public ActionBarAdvisor createActionBarAdvisor(IActionBarConfigurer configurer) {
-        return new ApplicationActionBarAdvisor(configurer);
-    }
-    
-    public void preWindowOpen() {
-        IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(400, 300));
-        configurer.setShowCoolBar(false);
-        configurer.setShowStatusLine(false);
-        configurer.setTitle("Hyperbola"); //$NON-NLS-1$
-    }
+	public ActionBarAdvisor createActionBarAdvisor(
+			IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
+	}
+
+	public void preWindowOpen() {
+		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
+		configurer.setInitialSize(new Point(400, 300));
+		configurer.setShowCoolBar(true);
+		configurer.setShowMenuBar(true);
+		configurer.setShowStatusLine(true);
+		configurer.setTitle("Hyperbola");
+	}
 }
